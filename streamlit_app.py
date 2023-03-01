78fe6edb3724099318ab738c56448a16d0720da4
Nora Keenan <53098218+noraekNC@users.noreply.github.com>
2023-03-01 17:06:44 -0500

    Add function to get fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
 
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -40,14 +45,13 @@ try:
 except URLError as e:
    streamlit.error()
 
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
+streamlit.stop()
 #New text input for value to pull from my_data_rows
 fruit_choice_snowflake = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('The user entered ', fruit_choice_snowflake)
